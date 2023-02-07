9212497ccaadf2d756c650de88ff37a7f627e4d4
brmcbrid <69523108+brmcbrid@users.noreply.github.com>
2023-02-07 11:32:36 -0500

    Create P1.cpp
    Starting template

---
+++ P1.cpp
@@ -0,0 +1,24 @@
+// Write a program to enter a base integer value and exponent integer value. 
+// Then using a loop, calculate the result of the base value raised to the 
+// power of the exponent value.  
+
+// Note: Do not use any <cmath> library functions in your solution.
+
+// Example:  Where base is 4 and exponent is 6 then  4^6 = 4 * 4 * 4 * 4 * 4 * 4 = 4096
+
+// Complete the template below by adding missing solution code
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    cout << "Enter a base value: ";
+    cin >> base;
+    cout << "Enter an exponent value: ";
+    cin >> exp;
+
+
+    cout << "Result is " << answer;
+    return 0;
+}
